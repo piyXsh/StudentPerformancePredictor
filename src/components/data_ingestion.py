3864b99500cce6f229a8ccccae66f3ad5ade5a70
piyXsh <piyushaggarwal584@gmail.com>
2024-07-25 16:58:06 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str= os.path.join('..','..','artifact',"train.csv")
@@ -20,6 +22,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
+            #read from any sorce may be api, or database or cloude or local machine
             df=pd.read_csv(r'D:\Piyush\Learn\ML\Student Performance Predictor\notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
@@ -28,6 +31,7 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train Test split initiated")
+
             train_set,test_set= train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
@@ -45,4 +49,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data=obj.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
